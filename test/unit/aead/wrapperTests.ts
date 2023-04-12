9a9c2644d25a23664d833e0c9a9410012a5ae32c
Mark Herrmann <mark.herrmann@mailbox.org>
2023-04-13 01:36:43 +0200

    fixes tests

--- test/unit/aead/wrapperTests.ts
+++ test/unit/aead/wrapperTests.ts
@@ -7,6 +7,8 @@ import { aeads, defaultAead, wrapper } from '../../../lib/aead/wrapper';
 
 const testVectors: Record<AeadSpecifier, Record<string, Buffer>> = {
   aesgcm: {
+    // Official test vector from https://luca-giuzzi.unibs.it/corsi/Support/papers-cryptography/gcm-spec.pdf
+    // Section B; Test Case 16
     key: Buffer.from('feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308', 'hex'),
     nonce: Buffer.from('cafebabefacedbaddecaf888', 'hex'),
     data: Buffer.from(
@@ -20,46 +22,86 @@ const testVectors: Record<AeadSpecifier, Record<string, Buffer>> = {
     ),
     authTag: Buffer.from('76fc6ece0f4e1768cddf8853bb2d551b', 'hex')
   },
-  aesctrhmac: {},
+
+  aesctrhmac: {
+    // Official test vector from https://datatracker.ietf.org/doc/html/rfc3686.html#page-9
+    // Test Vector #7
+    key: Buffer.from('776BEFF2851DB06F4C8A0542C8696F6C6A81AF1EEC96B4D37FC1D689E6C1C104776BEFF2851DB06F4C8A0542C8696F6C6A81AF1EEC96B4D37FC1D689E6C1C104', 'hex'),
+    nonce: Buffer.from('00000060DB5672C97AA8F0B200000001', 'hex'),
+    data: Buffer.from('53696E676C6520626C6F636B206D7367', 'hex'),
+    ciphertext: Buffer.from('145AD01DBF824EC7560863DC71E3E0C0', 'hex'),
+
+    // Defined additionalData and tested authTag manually, before it was hard coded here
+    additionalData: Buffer.from('010203', 'hex'),
+    authTag: Buffer.from('5E788FDB1290D3E4872BAE4BE3921D77D4E1AD72AEF06525649E10D7A8C03F90', 'hex')
+  },
   xchacha20poly1305: {}
 };
 
+const mockRandom = (testVector: Record<string, Buffer>): void => {
+  sinon.restore();
+  sinon.replace(crypto, 'randomBytes', (size: number): Buffer => size === testVector.nonce.length ? testVector.nonce : testVector.key);
+};
+
+const ensureRealRandom = (): void => {
+  sinon.restore();
+};
+
 suite('AEADs', (): void => {
-  test(`uses ${defaultAead} as default.`, async (): Promise<void> => {
-    const aead = defaultAead;
-    const testVector = testVectors.aesgcm;
-
-    const key = wrapper.keyGen(aead);
-    const nonce = wrapper.nonceGen(aead);
-    const { ciphertext, authTag } = wrapper.encrypt({ data: testVector.data, aead });
-    const decrypted = wrapper.decrypt({ key, nonce, ciphertext, authTag, aead });
-
-    assert.that(key).is.equalTo(testVector.key);
-    assert.that(nonce).is.equalTo(testVector.nonce);
-    assert.that(ciphertext).is.equalTo(testVector.ciphertext);
-    assert.that(decrypted).is.equalTo(testVector.data);
-  });
-
   for (const [ aeadName, aead ] of Object.entries(aeads)) {
     // TODO [2023-04-30]: remove this when all aeads implemented
-    if (aeadName !== 'aesgcm') {
+    if (aeadName === 'xchacha20poly1305') {
       return;
     }
 
     suite(`AEAD: ${aeadName}`, (): void => {
       const testVector = testVectors[aeadName as AeadSpecifier];
 
-      sinon.restore();
-      sinon.replace(crypto, 'randomBytes', (size: number): Buffer => size === testVector.nonce.length ? testVector.nonce : testVector.key);
+      suite('wrapper', (): void => {
+        if (aeadName === defaultAead) {
+          test(`uses ${defaultAead} as default.`, async (): Promise<void> => {
+            mockRandom(testVector);
+
+            const key = wrapper.keyGen();
+            const nonce = wrapper.nonceGen();
+            const { ciphertext, authTag } = wrapper.encrypt({ data: testVector.data });
+            const decrypted = wrapper.decrypt({ key, nonce, ciphertext, authTag });
+
+            assert.that(key).is.equalTo(testVector.key);
+            assert.that(nonce).is.equalTo(testVector.nonce);
+            assert.that(ciphertext).is.equalTo(testVector.ciphertext);
+            assert.that(decrypted).is.equalTo(testVector.data);
+          });
+        }
+
+        test(`uses ${aeadName} if specified.`, async (): Promise<void> => {
+          mockRandom(testVector);
+          const specifiedAead = aeadName as AeadSpecifier;
+
+          const key = wrapper.keyGen(specifiedAead);
+          const nonce = wrapper.nonceGen(specifiedAead);
+          const { ciphertext, authTag } = wrapper.encrypt({ data: testVector.data, aead: specifiedAead });
+          const decrypted = wrapper.decrypt({ key, nonce, ciphertext, authTag, aead: specifiedAead });
+
+          assert.that(key).is.equalTo(testVector.key);
+          assert.that(nonce).is.equalTo(testVector.nonce);
+          assert.that(ciphertext).is.equalTo(testVector.ciphertext);
+          assert.that(decrypted).is.equalTo(testVector.data);
+        });
+      });
 
       suite('generations', (): void => {
         test('returns correct key, using mocked random.', async (): Promise<void> => {
+          mockRandom(testVector);
+
           const generatedKey = aead.keyGen();
 
           assert.that(generatedKey).is.equalTo(testVector.key);
         });
 
         test('returns correct nonce, using mocked random.', async (): Promise<void> => {
+          mockRandom(testVector);
+
           const generatedNonce = aead.nonceGen();
 
           assert.that(generatedNonce).is.equalTo(testVector.nonce);
@@ -77,6 +119,7 @@ suite('AEADs', (): void => {
         });
 
         test('encrypts correctly using official testvector, no key given.', async (): Promise<void> => {
+          mockRandom(testVector);
           const { key, nonce, data, additionalData, ciphertext, authTag } = testVector;
 
           const result = aead.encrypt({ data, nonce, additionalData });
@@ -87,6 +130,7 @@ suite('AEADs', (): void => {
         });
 
         test('encrypts correctly using official testvector, no nonce given.', async (): Promise<void> => {
+          mockRandom(testVector);
           const { key, nonce, data, additionalData, ciphertext, authTag } = testVector;
 
           const result = aead.encrypt({ data, key, additionalData });
@@ -97,6 +141,7 @@ suite('AEADs', (): void => {
         });
 
         test('encrypts correctly using official testvector, no key and nonce given.', async (): Promise<void> => {
+          mockRandom(testVector);
           const { key, nonce, data, additionalData, ciphertext, authTag } = testVector;
 
           const result = aead.encrypt({ data, additionalData });
@@ -120,9 +165,8 @@ suite('AEADs', (): void => {
 
       suite('encryption and decryption, using random values', (): void => {
         test('encrypts and decrypts back correctly, given: data.', async (): Promise<void> => {
-          const data = Buffer.alloc(32);
-          // eslint-disable-next-line no-sync
-          crypto.randomFillSync(data);
+          ensureRealRandom();
+          const data = crypto.randomBytes(10);
 
           const { key, nonce, ciphertext, authTag } = aead.encrypt({ data });
           const decrypted = aead.decrypt({ key, nonce, ciphertext, authTag });
@@ -131,45 +175,47 @@ suite('AEADs', (): void => {
         });
 
         test('encrypts and decrypts back correctly, given: data, key.', async (): Promise<void> => {
-          const data = Buffer.alloc(32);
-          // eslint-disable-next-line no-sync
-          crypto.randomFillSync(data);
+          ensureRealRandom();
+          const data = crypto.randomBytes(10);
+          const key = crypto.randomBytes(testVector.key.length);
 
-          const { key, nonce, ciphertext, authTag } = aead.encrypt({ data, key: data });
+          const { nonce, ciphertext, authTag } = aead.encrypt({ data, key });
           const decrypted = aead.decrypt({ key, nonce, ciphertext, authTag });
 
           assert.that(decrypted).is.equalTo(data);
         });
 
         test('encrypts and decrypts back correctly, given: data, nonce.', async (): Promise<void> => {
-          const data = Buffer.alloc(32);
-          // eslint-disable-next-line no-sync
-          crypto.randomFillSync(data);
+          ensureRealRandom();
+          const data = crypto.randomBytes(10);
+          const nonce = crypto.randomBytes(testVector.nonce.length);
 
-          const { key, nonce, ciphertext, authTag } = aead.encrypt({ data, nonce: data });
+          const { key, ciphertext, authTag } = aead.encrypt({ data, nonce });
           const decrypted = aead.decrypt({ key, nonce, ciphertext, authTag });
 
           assert.that(decrypted).is.equalTo(data);
         });
 
         test('encrypts and decrypts back correctly, given: data, key, nonce.', async (): Promise<void> => {
-          const data = Buffer.alloc(32);
-          // eslint-disable-next-line no-sync
-          crypto.randomFillSync(data);
+          ensureRealRandom();
+          const data = crypto.randomBytes(10);
+          const key = crypto.randomBytes(testVector.key.length);
+          const nonce = crypto.randomBytes(testVector.nonce.length);
 
-          const { key, nonce, ciphertext, authTag } = aead.encrypt({ data, key: data, nonce: data });
+          const { ciphertext, authTag } = aead.encrypt({ data, key, nonce });
           const decrypted = aead.decrypt({ key, nonce, ciphertext, authTag });
 
           assert.that(decrypted).is.equalTo(data);
         });
 
         test('encrypts and decrypts back correctly, given: all.', async (): Promise<void> => {
-          const data = Buffer.alloc(32);
-          // eslint-disable-next-line no-sync
-          crypto.randomFillSync(data);
-          const additionalData = data;
+          ensureRealRandom();
+          const data = crypto.randomBytes(10);
+          const key = crypto.randomBytes(testVector.key.length);
+          const nonce = crypto.randomBytes(testVector.nonce.length);
+          const additionalData = crypto.randomBytes(3);
 
-          const { key, nonce, ciphertext, authTag } = aead.encrypt({ data, key: data, nonce: data, additionalData });
+          const { ciphertext, authTag } = aead.encrypt({ data, key, nonce, additionalData });
           const decrypted = aead.decrypt({ key, nonce, ciphertext, authTag, additionalData });
 
           assert.that(decrypted).is.equalTo(data);
