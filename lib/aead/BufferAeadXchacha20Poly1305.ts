79a13db91d3d1706e720f967ecdd0e40a38ebec0
Mark Herrmann <mark.herrmann@mailbox.org>
2023-04-15 23:14:16 +0200

    cleanup: no this -> static

--- lib/aead/BufferAeadXchacha20Poly1305.ts
+++ lib/aead/BufferAeadXchacha20Poly1305.ts
@@ -17,19 +17,18 @@ class BufferAeadXchacha20Poly1305 extends AbstractBufferAeadAesCcm {
   public encrypt (input: EncryptionInput): EncryptionOutput {
     const key = input.key ?? this.keyGen();
     const nonce = input.nonce ?? this.nonceGen();
-    const { key: tweakedKey, nonce: tweakedNonce } = this.tweakKeyAndNonce({ key, nonce });
+    const { key: tweakedKey, nonce: tweakedNonce } = BufferAeadXchacha20Poly1305.tweakKeyAndNonce({ key, nonce });
     const encryptionOutput = super.encrypt({ ...input, key: tweakedKey, nonce: tweakedNonce });
     return { ...encryptionOutput, key, nonce };
   }
 
   public decrypt (input: DecryptionInput): Buffer {
     const { key, nonce } = input;
-    const { key: tweakedKey, nonce: tweakedNonce } = this.tweakKeyAndNonce({ key, nonce });
+    const { key: tweakedKey, nonce: tweakedNonce } = BufferAeadXchacha20Poly1305.tweakKeyAndNonce({ key, nonce });
     return super.decrypt({ ...input, key: tweakedKey, nonce: tweakedNonce });
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  private tweakKeyAndNonce ({ key, nonce }: KeyAndNonce): KeyAndNonce {
+  private static tweakKeyAndNonce ({ key, nonce }: KeyAndNonce): KeyAndNonce {
     const hchachaNonce = nonce.subarray(0, 16);
     let tweakedKey = new Uint8Array(32);
     tweakedKey = hchacha(key, hchachaNonce, tweakedKey);
